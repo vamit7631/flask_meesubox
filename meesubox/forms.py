44ba0b61ddb4f96b17804ce454bf1ba87fd063f9
vamit7632 <vamit7632@gmail.com>
2021-04-21 14:40:22 +0530

    fourth commit

--- meesubox/forms.py
+++ meesubox/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-
+from wtforms.validators import Length, EqualTo, Email, DataRequired
 
 class RegisterForm(FlaskForm):
-    firstname = StringField(label='First Name')
-    lastname = StringField(label='Last Name')
-    mobile_no = StringField(label='Mobile No')
-    email_id = StringField(label='Email Address')
-    password = PasswordField(label='Password')
+    firstname = StringField(label='First Name', validators=[Length(min=2, max=30), DataRequired()])
+    lastname = StringField(label='Last Name', validators=[Length(min=2, max=30), DataRequired()])
+    mobile_no = StringField(label='Mobile No', validators=[Length(min=10, max=10), DataRequired()])
+    email_id = StringField(label='Email Address', validators=[Email(), DataRequired()])
+    password = PasswordField(label='Password', validators=[Length(min=8), DataRequired()])
     submit = SubmitField(label='Sign Up')
--- meesubox/routes.py
+++ meesubox/routes.py
@@ -19,11 +19,12 @@ def signin():
 def signup():
     form = RegisterForm()
     if form.validate_on_submit():
-        print(form, "-----------------------------Amit")
         new_user_record = UserModel(firstname = form.firstname.data, lastname = form.lastname.data, mobile_no = form.mobile_no.data, email_id = form.email_id.data, password = form.password.data )
-        db.session.add(new_user_record)
-        db.session.commit()
-    if form.errors != {}: #If there are not errors from the validations
-        for err_msg in form.errors.values():
-            print(f'There was an error with creating a user: {err_msg}')    
-    return render_template('signup.html', form=form)    
+        try:
+            db.session.add(new_user_record)
+            db.session.commit()
+            return redirect('/')
+        except:
+            print('There was an error with creating a user')   
+    else:
+        return render_template('signup.html', form=form)    
