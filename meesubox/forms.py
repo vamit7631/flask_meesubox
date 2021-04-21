5e21d34c1037842b26d106143274bf3f71bcdf40
vamit7632 <vamit7632@gmail.com>
2021-04-21 22:33:31 +0530

    fifth commit

--- meesubox/forms.py
+++ meesubox/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import Length, EqualTo, Email, DataRequired
+from meesubox.models import UserModel
 
 class RegisterForm(FlaskForm):
     firstname = StringField(label='First Name', validators=[Length(min=2, max=30), DataRequired()])
@@ -9,3 +10,8 @@ class RegisterForm(FlaskForm):
     email_id = StringField(label='Email Address', validators=[Email(), DataRequired()])
     password = PasswordField(label='Password', validators=[Length(min=8), DataRequired()])
     submit = SubmitField(label='Sign Up')
+
+class LoginForm(FlaskForm):
+    email_id = StringField(label='Email Address', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
+    submit = SubmitField(label='Login')    
--- meesubox/routes.py
+++ meesubox/routes.py
@@ -1,19 +1,13 @@
 from meesubox import app
-from flask import render_template, redirect, url_for
+from flask import render_template, redirect, url_for, flash
 from meesubox.models import UserModel
-from meesubox.forms import RegisterForm
+from meesubox.forms import RegisterForm , LoginForm
 from meesubox import db
 
 @app.route('/')
 @app.route('/home')
 def home_page():
     return render_template('home.html')
-
-
-@app.route('/signin')
-def signin():
-    return render_template('signin.html')
-
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -28,3 +22,21 @@ def signup():
             print('There was an error with creating a user')   
     else:
         return render_template('signup.html', form=form)    
+
+
+
+
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user_check = UserModel.query.filter_by(email_id = form.email_id.data).first()
+        if user_check and user_check.check_password_correction(
+            attempted_password = form.password.data
+        ):    
+            login_user(user_check)
+            flash(f'Success! You are logged in as: {user_check.email_id}', category='success')
+            return redirect(url_for('/'))
+        else:
+            flash('email and password are not match! Please try again', category='danger')
+    return render_template('signin.html', form=form)
