cf16aef563e2aac7ae84e3801b0ccb20d6d78776
vamit7632 <vamit7632@gmail.com>
2021-04-22 14:05:44 +0530

    seventh commit

--- meesubox/routes.py
+++ meesubox/routes.py
@@ -7,15 +7,14 @@ from meesubox import db
 from flask_login import login_user, logout_user, login_required, current_user
 
 @app.route('/')
-@app.route('/home')
 def home_page():
     return render_template('home.html')
 
-@app.route('/index')
+@app.route('/home')
 @login_required
-def index():
+def login_home_page():
     # print(current_user.firstname,"-------------------------test")
-    return render_template('index.html', name=current_user.firstname)    
+    return render_template('home.html', name=current_user.firstname)    
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
@@ -36,24 +35,34 @@ def signup():
 
 @app.route('/signin', methods=['GET', 'POST'])
 def signin():
-    if not current_user.is_authenticated:
+    if current_user.is_authenticated:
+        return redirect(url_for('login_home_page'))
+    else:
         form = LoginForm()
-        if form.validate_on_submit():
+        if form.validate_on_submit(): 
             user = UserModel.query.filter_by(email_id = form.email_id.data).first()
             if not user or not check_password_hash(user.password, form.password.data):   
                 return redirect(url_for('signin'))
-        
-            login_user(user, remember=True)
-            return redirect(url_for('index'))
-        return render_template('signin.html', form=form)
-    
-    else:
-        return redirect(url_for('index'))
 
+            login_user(user, remember=True)
+            return redirect(url_for('login_home_page'))
+        return render_template('signin.html', form=form)    
 
 @app.route('/signout')
 @login_required
 def logout_page():
     logout_user()
     return redirect(url_for("home_page"))
+
+
+
+@app.route('/wishlist')
+@login_required
+def wishlist():
+    return render_template('wishlist.html')    
+
+@app.route('/cart-details')
+@login_required
+def cart_details():
+    return render_template('shopping-cart.html')    
   
